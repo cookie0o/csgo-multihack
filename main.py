431bddabed04f418e5f3824e71c7a8c83662fa06
cookie0_o <81589649+cookie0o@users.noreply.github.com>
2022-02-20 05:37:26 +0100

    v1.1
    new features!
    **look in readme.md** for more information!

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #imports
-import webbrowser,time,keyboard,numpy as np,pymem,pymem.process
+import webbrowser
+import numpy as np
+import pymem
+import pymem.process
+import keyboard
+import time
+import pymem
 from PyQt5 import QtCore,QtGui,QtWidgets
 from threading import Thread
 
@@ -7,11 +13,20 @@ from threading import Thread
 from offsets import *
 
 #github link and version
-github_url='https://github.com/cookie_o'
-CURRENT_VERSION='1._'
+github_url='https://github.com/cookie0o'
+CURRENT_VERSION='1.1'
+
+
+#PATHS
+#current dir
+dirname = os.path.dirname(__file__)
+
+#path to sounds
+sounds_path = os.path.join(dirname, 'sounds/')
 
 
 
+#RUN CHEAT LOOP
 def main(self):
 
     try:
@@ -19,13 +34,17 @@ def main(self):
         client=pymem.process.module_from_name(pm.process_handle,'client.dll').lpBaseOfDll
         engine=pymem.process.module_from_name(pm.process_handle,'engine.dll').lpBaseOfDll
         enginepointer=pm.read_int(engine+dwClientState)
+        player = pm.read_int(client + dwLocalPlayer)
+        hitsound = pm.read_int(player + m_totalHitsOnServer)
     except pymem.exception.ProcessNotFound:
         pass
 
-
+    zoom_on_in = 0
+    zoom_on_out = 0
+    
     while True:
   
-      time.sleep(0.01)
+      time.sleep(0.001)
       BUNNYHOP_on_off=0
       if self.BUNNYHOP_ON_checkbox.isChecked():
         BUNNYHOP_on_off=1
@@ -82,6 +101,115 @@ def main(self):
         pass
 
 
+
+      #FOV CHANGER
+      
+      #get values;
+      #get zoom out value;
+      FOV_OUT_VALUE_=self.FOV_OUT_VALUE.text()
+      #convert str to float
+      FOV_OUT_VALUE_FLOAT = float(FOV_OUT_VALUE_)
+      #get fov in value;
+      FOV_IN_VALUE_=self.FOV_IN_VALUE.text()
+      #convert str to float
+      FOV_IN_VALUE_FLOAT = float(FOV_IN_VALUE_)
+
+      #get toggle keys;
+
+      #get zoom in toggle key;
+      ZOOM_IN_FOV_toggle_key_=self.ZOOM_IN_FOV_toggle_key.text()
+
+      #get zoom out toggle key;
+      ZOOM_OUT_FOV_toggle_key_=self.ZOOM_OUT_FOV_toggle_key.text()
+
+
+      
+      #ZOOM IN
+      if keyboard.is_pressed(ZOOM_IN_FOV_toggle_key_) and self.ZOOM_IN_FOV_ON_checkbox.isChecked():
+        if zoom_on_in == 0:
+          zoom_on_in = 2
+
+          time.sleep(0.1)
+
+          #ZOOM
+          inputFOV = int(round(FOV_IN_VALUE_FLOAT, 0))
+          player = pm.read_int(client + dwLocalPlayer)
+          inputFOV = int(round(FOV_IN_VALUE_FLOAT, 0))
+
+          fov = player + m_iFOV
+          pm.write_int(fov, inputFOV)
+
+      else:
+        pass
+        
+      if keyboard.is_pressed(ZOOM_IN_FOV_toggle_key_) and zoom_on_in == 2:
+        time.sleep(0.1)
+        zoom_on_in = 0
+        #reset zoom
+        player = pm.read_int(client + dwLocalPlayer)
+        fov = player + m_iFOV
+        pm.write_int(fov, 90)
+      else:
+        pass
+
+      #ZOOM OUT
+      if keyboard.is_pressed(ZOOM_OUT_FOV_toggle_key_) and self.ZOOM_OUT_FOV_ON_checkbox.isChecked():
+        if zoom_on_out == 0:
+          zoom_on_out = 2
+
+          time.sleep(0.1)
+
+          #ZOOM
+          inputFOV = int(round(FOV_OUT_VALUE_FLOAT, 0))
+          player = pm.read_int(client + dwLocalPlayer)
+          inputFOV = int(round(FOV_OUT_VALUE_FLOAT, 0))
+          fov = player + m_iFOV
+          pm.write_int(fov, inputFOV)
+
+      else:
+        pass
+
+      if keyboard.is_pressed(ZOOM_OUT_FOV_toggle_key_) and zoom_on_out == 2:
+        time.sleep(0.1)
+        zoom_on_out = 0
+        #reset zoom
+        player = pm.read_int(client + dwLocalPlayer)
+        fov = player + m_iFOV
+        pm.write_int(fov, 90)
+      else:
+        pass
+
+      
+      #TRIGGER BOT
+      
+      #get trigger key
+      TRIGGERBOT_hold_key_=self.TRIGGERBOT_hold_key.text()
+
+      #get shoot delay
+      TRIGGERBOT_DELAY_=self.TRIGGERBOT_DELAY.text()
+
+      TRIGGERBOT_DELAY_FLOAT = float(TRIGGERBOT_DELAY_)
+
+
+      if self.TRIGGERBOT_ON_checkbox.isChecked():
+        if keyboard.is_pressed(TRIGGERBOT_hold_key_):
+            player = pm.read_int(client + dwLocalPlayer)
+            entity_id = pm.read_int(player + m_iCrosshairId)
+            entity = pm.read_int(client + dwEntityList + (entity_id - 1) * 0x10)
+
+            entity_team = pm.read_int(entity + m_iTeamNum)
+            player_team = pm.read_int(player + m_iTeamNum)
+
+            if entity_id > 0 and entity_id <= 64 and player_team != entity_team:
+              time.sleep(TRIGGERBOT_DELAY_FLOAT)
+              pm.write_int(client + dwForceAttack, 6) 
+
+            else:
+              pass
+        else:
+          pass
+
+
       #T
       T_RED_VALUE_=self.T_RED_VALUE.text()
       T_GREEN_VALUE_=self.T_GREEN_VALUE.text()
@@ -119,7 +247,7 @@ def main(self):
           entity_glow=pm.read_int(entity+m_iGlowIndex)
 
 
-          if ESP_t_on_off==1:    
+          if ESP_t_on_off==1:
             if entity_team_id==2:
               pm.write_float(glow_manager+entity_glow*56+8,float(T_R))
               pm.write_float(glow_manager+entity_glow*56+12,float(T_G))
@@ -442,21 +570,185 @@ class Ui_main_window(object):
         self.label_2.setStyleSheet("color: rgb(200, 0, 200);")
         self.label_2.setObjectName("label_2")
         self.tabWidget.addTab(self.misc_tab, "")
-
+        self.fov_tab = QtWidgets.QWidget()
+        self.fov_tab.setObjectName("fov_tab")
+        self.zoom_out_frame = QtWidgets.QFrame(self.fov_tab)
+        self.zoom_out_frame.setGeometry(QtCore.QRect(10, 10, 521, 111))
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        self.zoom_out_frame.setFont(font)
+        self.zoom_out_frame.setStyleSheet("")
+        self.zoom_out_frame.setFrameShape(QtWidgets.QFrame.Box)
+        self.zoom_out_frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.zoom_out_frame.setLineWidth(2)
+        self.zoom_out_frame.setObjectName("zoom_out_frame")
+        self.ZOOM_OUT_FOV_ON_checkbox = QtWidgets.QCheckBox(self.zoom_out_frame)
+        self.ZOOM_OUT_FOV_ON_checkbox.setGeometry(QtCore.QRect(10, 10, 181, 21))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.ZOOM_OUT_FOV_ON_checkbox.setFont(font)
+        self.ZOOM_OUT_FOV_ON_checkbox.setChecked(True)
+        self.ZOOM_OUT_FOV_ON_checkbox.setObjectName("ZOOM_OUT_FOV_ON_checkbox")
+        self.FOV_OUT_VALUE = QtWidgets.QLineEdit(self.zoom_out_frame)
+        self.FOV_OUT_VALUE.setGeometry(QtCore.QRect(10, 30, 151, 41))
+        font = QtGui.QFont()
+        font.setPointSize(27)
+        font.setBold(True)
+        font.setWeight(75)
+        self.FOV_OUT_VALUE.setFont(font)
+        self.FOV_OUT_VALUE.setMaxLength(3)
+        self.FOV_OUT_VALUE.setAlignment(QtCore.Qt.AlignCenter)
+        self.FOV_OUT_VALUE.setObjectName("FOV_OUT_VALUE")
+        self.label_5 = QtWidgets.QLabel(self.zoom_out_frame)
+        self.label_5.setGeometry(QtCore.QRect(10, 70, 111, 31))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.ZOOM_OUT_FOV_toggle_key = QtWidgets.QLineEdit(self.zoom_out_frame)
+        self.ZOOM_OUT_FOV_toggle_key.setGeometry(QtCore.QRect(100, 70, 61, 31))
+        font = QtGui.QFont()
+        font.setPointSize(19)
+        self.ZOOM_OUT_FOV_toggle_key.setFont(font)
+        self.ZOOM_OUT_FOV_toggle_key.setMaxLength(333)
+        self.ZOOM_OUT_FOV_toggle_key.setAlignment(QtCore.Qt.AlignCenter)
+        self.ZOOM_OUT_FOV_toggle_key.setObjectName("ZOOM_OUT_FOV_toggle_key")
+        self.FOV_130_IMG = QtWidgets.QLabel(self.zoom_out_frame)
+        self.FOV_130_IMG.setGeometry(QtCore.QRect(360, 10, 151, 91))
+        self.FOV_130_IMG.setStyleSheet("image: url(:/fov_images/130_fov.png);")
+        self.FOV_130_IMG.setText("")
+        self.FOV_130_IMG.setScaledContents(True)
+        self.FOV_130_IMG.setObjectName("FOV_130_IMG")
+        self.label_6 = QtWidgets.QLabel(self.zoom_out_frame)
+        self.label_6.setGeometry(QtCore.QRect(160, 70, 171, 31))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_6.setFont(font)
+        self.label_6.setObjectName("label_6")
+        self.zoom_in_frame = QtWidgets.QFrame(self.fov_tab)
+        self.zoom_in_frame.setGeometry(QtCore.QRect(10, 130, 521, 111))
+        self.zoom_in_frame.setFrameShape(QtWidgets.QFrame.Box)
+        self.zoom_in_frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.zoom_in_frame.setLineWidth(2)
+        self.zoom_in_frame.setObjectName("zoom_in_frame")
+        self.ZOOM_IN_FOV_ON_checkbox = QtWidgets.QCheckBox(self.zoom_in_frame)
+        self.ZOOM_IN_FOV_ON_checkbox.setGeometry(QtCore.QRect(10, 10, 181, 21))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.ZOOM_IN_FOV_ON_checkbox.setFont(font)
+        self.ZOOM_IN_FOV_ON_checkbox.setChecked(True)
+        self.ZOOM_IN_FOV_ON_checkbox.setObjectName("ZOOM_IN_FOV_ON_checkbox")
+        self.FOV_IN_VALUE = QtWidgets.QLineEdit(self.zoom_in_frame)
+        self.FOV_IN_VALUE.setGeometry(QtCore.QRect(10, 30, 151, 41))
+        font = QtGui.QFont()
+        font.setPointSize(27)
+        font.setBold(True)
+        font.setWeight(75)
+        self.FOV_IN_VALUE.setFont(font)
+        self.FOV_IN_VALUE.setMaxLength(3)
+        self.FOV_IN_VALUE.setAlignment(QtCore.Qt.AlignCenter)
+        self.FOV_IN_VALUE.setObjectName("FOV_IN_VALUE")
+        self.label_7 = QtWidgets.QLabel(self.zoom_in_frame)
+        self.label_7.setGeometry(QtCore.QRect(10, 70, 111, 31))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.label_7.setFont(font)
+        self.label_7.setObjectName("label_7")
+        self.ZOOM_IN_FOV_toggle_key = QtWidgets.QLineEdit(self.zoom_in_frame)
+        self.ZOOM_IN_FOV_toggle_key.setGeometry(QtCore.QRect(100, 70, 61, 31))
+        font = QtGui.QFont()
+        font.setPointSize(19)
+        self.ZOOM_IN_FOV_toggle_key.setFont(font)
+        self.ZOOM_IN_FOV_toggle_key.setMaxLength(333)
+        self.ZOOM_IN_FOV_toggle_key.setAlignment(QtCore.Qt.AlignCenter)
+        self.ZOOM_IN_FOV_toggle_key.setObjectName("ZOOM_IN_FOV_toggle_key")
+        self.FOV_80_IMG = QtWidgets.QLabel(self.zoom_in_frame)
+        self.FOV_80_IMG.setGeometry(QtCore.QRect(360, 10, 151, 91))
+        self.FOV_80_IMG.setStyleSheet("image: url(:/fov_images/80_fov.png);")
+        self.FOV_80_IMG.setText("")
+        self.FOV_80_IMG.setScaledContents(True)
+        self.FOV_80_IMG.setObjectName("FOV_80_IMG")
+        self.label_8 = QtWidgets.QLabel(self.zoom_in_frame)
+        self.label_8.setGeometry(QtCore.QRect(160, 70, 181, 31))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.label_8.setFont(font)
+        self.label_8.setObjectName("label_8")
+        self.zoom_in_frame.raise_()
+        self.zoom_out_frame.raise_()
+        self.tabWidget.addTab(self.fov_tab, "")
+        self.triggerbot_tab = QtWidgets.QWidget()
+        self.triggerbot_tab.setObjectName("triggerbot_tab")
+        self.frame_2 = QtWidgets.QFrame(self.triggerbot_tab)
+        self.frame_2.setGeometry(QtCore.QRect(10, 10, 511, 221))
+        self.frame_2.setFrameShape(QtWidgets.QFrame.Box)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_2.setLineWidth(2)
+        self.frame_2.setObjectName("frame_2")
+        self.TRIGGERBOT_ON_checkbox = QtWidgets.QCheckBox(self.frame_2)
+        self.TRIGGERBOT_ON_checkbox.setGeometry(QtCore.QRect(10, 10, 171, 41))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        font.setBold(True)
+        font.setWeight(75)
+        self.TRIGGERBOT_ON_checkbox.setFont(font)
+        self.TRIGGERBOT_ON_checkbox.setObjectName("TRIGGERBOT_ON_checkbox")
+        self.label_10 = QtWidgets.QLabel(self.frame_2)
+        self.label_10.setGeometry(QtCore.QRect(10, 50, 111, 41))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label_10.setFont(font)
+        self.label_10.setObjectName("label_10")
+        self.TRIGGERBOT_hold_key = QtWidgets.QLineEdit(self.frame_2)
+        self.TRIGGERBOT_hold_key.setGeometry(QtCore.QRect(90, 50, 81, 41))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        font.setBold(True)
+        font.setWeight(75)
+        self.TRIGGERBOT_hold_key.setFont(font)
+        self.TRIGGERBOT_hold_key.setMaxLength(333)
+        self.TRIGGERBOT_hold_key.setAlignment(QtCore.Qt.AlignCenter)
+        self.TRIGGERBOT_hold_key.setObjectName("TRIGGERBOT_hold_key")
+        self.label_3 = QtWidgets.QLabel(self.frame_2)
+        self.label_3.setGeometry(QtCore.QRect(10, 90, 331, 31))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.TRIGGERBOT_DELAY = QtWidgets.QLineEdit(self.frame_2)
+        self.TRIGGERBOT_DELAY.setGeometry(QtCore.QRect(10, 120, 101, 31))
+        font = QtGui.QFont()
+        font.setPointSize(17)
+        font.setBold(True)
+        font.setWeight(75)
+        self.TRIGGERBOT_DELAY.setFont(font)
+        self.TRIGGERBOT_DELAY.setObjectName("TRIGGERBOT_DELAY")
+        self.label_4 = QtWidgets.QLabel(self.frame_2)
+        self.label_4.setGeometry(QtCore.QRect(110, 120, 61, 31))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.tabWidget.addTab(self.triggerbot_tab, "")
 
 
         #BUTTONS
 
-        #github open button
+        #open github button
         self.github_link_button.clicked.connect(lambda: webbrowser.open_new(github_url))
-
 
         #inject button
         self.inject_button.clicked.connect(lambda: Thread(target=main_start(self)).start())
 
 
         self.retranslateUi(main_window)
-        self.tabWidget.setCurrentIndex(2)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(main_window)
 
     def retranslateUi(self, main_window):
@@ -495,6 +787,24 @@ class Ui_main_window(object):
 " pls star this project on github\n"
 " much love :)"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.misc_tab), _translate("main_window", "misc"))
+        self.ZOOM_OUT_FOV_ON_checkbox.setText(_translate("main_window", "zoom out FOV;"))
+        self.FOV_OUT_VALUE.setText(_translate("main_window", "130"))
+        self.label_5.setText(_translate("main_window", "toggle Key:"))
+        self.ZOOM_OUT_FOV_toggle_key.setText(_translate("main_window", "F7"))
+        self.label_6.setText(_translate("main_window", "press key again to reset FOV!"))
+        self.ZOOM_IN_FOV_ON_checkbox.setText(_translate("main_window", "zoom in FOV;"))
+        self.FOV_IN_VALUE.setText(_translate("main_window", "75"))
+        self.label_7.setText(_translate("main_window", "toggle Key:"))
+        self.ZOOM_IN_FOV_toggle_key.setText(_translate("main_window", "F8"))
+        self.label_8.setText(_translate("main_window", "press key again to reset FOV!"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.fov_tab), _translate("main_window", "fov"))
+        self.TRIGGERBOT_ON_checkbox.setText(_translate("main_window", "Triggerbot"))
+        self.label_10.setText(_translate("main_window", "hold Key:"))
+        self.TRIGGERBOT_hold_key.setText(_translate("main_window", "ALT"))
+        self.label_3.setText(_translate("main_window", "shoot delay (if you want to be a little legit);"))
+        self.TRIGGERBOT_DELAY.setText(_translate("main_window", "0"))
+        self.label_4.setText(_translate("main_window", "seconds"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.triggerbot_tab), _translate("main_window", "triggerbot"))
 
 import _res_.res_rc
 if __name__=='__main__':
